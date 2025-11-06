9b6f0e946d5154185528b083b55b4db406151f0a
SumanovaViktoriia <Visumanova@gmail.com>
2025-11-06 14:46:12 +0300

    1-6 пункты adc

---
+++ get-dac/dac.py
@@ -0,0 +1,29 @@
+#1
+
+class R2R_ADC:
+    def __init__(self, dynamic_range, compare_time = 0.01, verbose = False):
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+        self.compare_time = compare_time
+        
+        self.bits_gpio = [26, 20, 19, 16, 13, 12, 25, 11]
+        self.comp_gpio = 21
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.bits_gpio, GPIO.OUT, initial = 0)
+        GPIO.setup(self.comp_gpio, GPIO.IN)
+
+
+#2
+
+def plot_voltage_vs_time(time, voltage, max_voltage):
+
+plt.figure(figsize=(10,6))
+
+plt.plot(time, voltage)
+
+plt.show()
+
+voltage_values = []
+time_values = []
+duration = 3.0
